1504a5d3f1c6fcde61cf58988b49df95b71b0193
ktakesue <ktakesue@aol.com>
2018-01-24 13:36:56 -1000

    chirp exercise done

--- js/chirp.js
+++ js/chirp.js
@@ -1,5 +1,14 @@
 function chirp(n){
   // FIX ME
+//   if (n === 0){
+//      return;
+//   } 
+//   return "chirp " + chirp(--n);
+// }
+  if(n === 1){
+    return "chirp ";
+  }
+  return "chirp " + chirp(n - 1);
 }
 
 $(document).ready(function(){
